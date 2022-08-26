80054312314a20620420c4a9a8476d8fad027e1f
armansargsyan <armansargsyan9991@gmail.com>
2022-08-27 00:27:39 +0400

    added styles, and functionality(pagination, infinity scroll) resolved bugs, remains bug with infinity scrolling and text update functionality

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 class DraggingEvent {
+    dragStarted = false;
     constructor(target = undefined) {
         this.target = target;
     }
 
     event(callback) {
-        let handler;
+        let handler, moveHandler;
 
         this.target.addEventListener("mousedown", e => {
             this.target.classList.add('dragging');
             e.preventDefault();
 
             handler = callback(e);
+            moveHandler = (e) => {
+                // need to disable click handler in drag case
+                this.dragStarted = true;
+                handler(e)
+            };
 
             const clearDraggingEvent = () => {
                 this.target.classList.remove('dragging');
 
-                window.removeEventListener("mousemove", handler);
+                window.removeEventListener("mousemove", moveHandler);
                 window.removeEventListener("mouseup", clearDraggingEvent);
 
                 document.removeEventListener("mouseleave", clearDraggingEvent);
@@ -23,7 +29,7 @@ class DraggingEvent {
                 handler(null)
             };
 
-            window.addEventListener("mousemove", handler);
+            window.addEventListener("mousemove", moveHandler);
 
             document.addEventListener("mouseleave", clearDraggingEvent);
 
@@ -36,17 +42,22 @@ class DraggingEvent {
             this.target.classList.add('dragging');
 
             handler = callback(e);
+            moveHandler = (e) => {
+                // need to disable click handler in drag case
+                this.dragStarted = true;
+                handler(e)
+            };
 
             const clearDraggingEvent = () => {
                 this.target.classList.remove('dragging');
 
-                window.removeEventListener("touchmove", handler);
+                window.removeEventListener("touchmove", moveHandler);
                 window.removeEventListener("touchend", clearDraggingEvent);
 
                 handler(null)
             };
 
-            window.addEventListener("touchmove", handler);
+            window.addEventListener("touchmove", moveHandler);
 
             window.addEventListener("touchend", clearDraggingEvent);
 
@@ -54,6 +65,10 @@ class DraggingEvent {
 
 
         })
+    }
+
+    dragEnd() {
+        this.dragStarted = false;
     }
 
     // Get the distance that the user has dragged
@@ -98,17 +113,23 @@ class CarouselSetting {
     dataSource;
     cardMargin;
     defClass;
+    showedSlidesCount;
+    paginationSize;
 
     constructor(
         {
             dataSource,
             cardMargin,
-            defClass
+            defClass,
+            showedSlidesCount,
+            paginationSize
         }
     ) {
         this.dataSource = dataSource;
         this.cardMargin = cardMargin;
         this.defClass = defClass;
+        this.showedSlidesCount = showedSlidesCount;
+        this.paginationSize = paginationSize;
     }
 
     getTemplate() {
@@ -122,12 +143,14 @@ class CarouselSetting {
 
 class Carousel extends DraggingEvent {
 
-    constructor(container, settings) {
-        super(container);
-        this.container = container;
+    constructor(elements, settings) {
+        super(elements.container);
+        this.container = elements.container;
         this.settings = settings;
 
-        this.init();
+        this.init(elements);
+
+
     }
 
     drag = (changedPosition) => {
@@ -140,24 +163,49 @@ class Carousel extends DraggingEvent {
         }
     };
 
-    init() {
+    init(elements) {
         this.container.innerHTML = this.settings.getTemplate();
         this.activeIndex = 0;
         this.calcCardsWith();
         this.getCards();
         this.setCardsDefaultSettings();
+        this.setCardsClickHandler();
+        this.initControls(elements);
         this.firstRender();
         this.getDistance(this.drag);
+        window.addEventListener("resize", this.updateCardWidth)
+
     }
+
+    initControls(elements) {
+        elements.leftButton.onclick = () => this.slideStep(-1);
+        elements.rightButton.onclick = () => this.slideStep(1);
+        elements.pagination.style.width = `${this.cards.length * this.settings.paginationSize}px`;
+        elements.pagination.onclick = (e) => this.paginationClick(e);
+        this.paginationIndicator = elements.pagination.querySelector('span');
+        this.paginationIndicator.onclick = (e) => {
+            e.stopPropagation();
+        };
+        this.paginationIndicator.style.width = `${this.settings.paginationSize}px`;
+    }
+
+    updateCardWidth = () => {
+        this.calcCardsWith();
+        this.setCardsDefaultSettings();
+        this.firstRender();
+
+    };
 
     firstRender() {
         this.cards.forEach((card, index) => {
             this.renderItem(card, index - this.activeIndex);
-        })
+        });
+        this.renderPagination()
     }
 
     calcCardsWith() {
-        this.cardSize = (this.container.offsetWidth - 2 * this.settings.cardMargin) * 4 / 7;
+        const slidesCount = this.settings.showedSlidesCount;
+        this.cardSize = (this.container.offsetWidth - (slidesCount - 1) * this.settings.cardMargin) * 2 ** (slidesCount - 1) / (2 ** slidesCount - 1);
         if (this.cardSize > this.container.offsetHeight) {
             this.cardSize = this.container.offsetHeight;
         }
@@ -168,28 +216,48 @@ class Carousel extends DraggingEvent {
     }
 
     setCardsDefaultSettings() {
-        this.cards.forEach((card, index) => {
+        this.cards.forEach((card) => {
             card.style.width = `${this.cardSize}px`;
             card.style.height = `${this.cardSize}px`;
+        });
+    }
+
+    setCardsClickHandler() {
+        this.cards.forEach((card, index) => {
             card.onclick = () => this.setToActive(index);
-        })
+        });
     }
 
     renderItem(card, deltaI) {
+        let isTransitionOff = false;
+
+        if (deltaI < this.settings.showedSlidesCount - this.cards.length) {
+            deltaI = deltaI + this.cards.length;
+            isTransitionOff = true;
+        } else if (deltaI > this.cards.length - 1) {
+
+            deltaI = deltaI - this.cards.length;
+            isTransitionOff = true;
+        }
         const newScale = this.calcScale(deltaI),
             newPosition = this.calcPosition(deltaI);
+        // card.style.zIndex = `${zIndex}`;
+        if (isTransitionOff) {
+            // card.style.transitionDelay = '0s';
+        }
         card.style.left = `${newPosition}px`;
         card.style.transform = `scale(${newScale})`;
         card.setAttribute('data-scale', newScale);
+        if (isTransitionOff) {
+        }
     }
 
     calcPosition(i) {
-        // if (i < -1) i = -1;
         return (2 ** i - 1) / (2 ** (i - 1)) * this.cardSize + i * this.settings.cardMargin;
     }
 
     calcScale(i) {
-        // if (i < -1) i = -1;
+        if (i <= 0) return 1;
         return 1 / (2 ** i);
     }
 
@@ -197,16 +265,29 @@ class Carousel extends DraggingEvent {
         this.cards.forEach((card, index) => {
             this.renderItem(card, index + deltaX - this.activeIndex);
         });
+        this.renderPagination();
+
     }
 
     setToActive(index) {
+        if (this.dragStarted) {
+            super.dragEnd();
+            return;
+        }
         this.updateActive(index);
         this.render();
     }
 
     slideStep(step) {
-        this.updateActive(this.activeIndex + step);
-        this.render();
+        let newActiveIndex = this.activeIndex + step;
+        const count = Math.floor(newActiveIndex / this.cards.length);
+        if (Math.abs(count) >= 1) {
+            newActiveIndex = newActiveIndex - count * this.cards.length;
+        }
+        if (newActiveIndex !== this.activeIndex) {
+            this.updateActive(newActiveIndex);
+            this.render()
+        }
     }
 
     calcActiveIndex() {
@@ -223,9 +304,20 @@ class Carousel extends DraggingEvent {
             this.updateActive(minIndex);
         }
     }
+    paginationClick(e) {
+        const index = Math.floor(e.offsetX / this.settings.paginationSize);
+        if (index !== this.activeIndex) {
+            this.setToActive(index);
+        }
+    }
 
     updateActive(index) {
         this.activeIndex = index;
+    }
+
+    renderPagination(step = this.activeIndex) {
+        let position = step * this.settings.paginationSize;
+        this.paginationIndicator.style.left = `${position}px`
     }
 
 }
@@ -242,19 +334,26 @@ const carouselSetting = new CarouselSetting({
         },
         {
             text: 4
-        }
+        },
+        // {
+        //     text: 5
+        // },
+        // {
+        //     text: 6
+        // },
+        // {
+        //     text: 7
+        // }
     ],
     cardMargin: 40,
-    defClass: 'carousel-item'
+    defClass: 'carousel-item',
+    showedSlidesCount: 3,
+    paginationSize: 50
 });
-const carouselContainer = document.querySelector('.container');
-const carouselInstance = new Carousel(carouselContainer, carouselSetting);
-
-function slideToLeft() {
-    carouselInstance.slideStep(-1);
-}
-
-function slideToRight() {
-    carouselInstance.slideStep(1);
-
-}
+const carouselElements = {
+    container: document.querySelector('.container'),
+    pagination: document.querySelector('.pagination'),
+    leftButton: document.querySelector('.leftButton'),
+    rightButton: document.querySelector('.rightButton'),
+};
+const carouselInstance = new Carousel(carouselElements, carouselSetting);
